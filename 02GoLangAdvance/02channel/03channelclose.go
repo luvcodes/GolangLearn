afbc339d356383110cd53b8355424e3b1b2e402d
Runze Yang <ryanyyyyyyy@163.com>
2024-06-12 10:27:27 +1000

    close channel functionality

---
+++ 02GoLangAdvance/02channel/03channelclose.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	c := make(chan int)
+
+	go func() {
+		for i := 0; i < 5; i++ {
+			c <- i
+		}
+
+		// close可以关闭一个channel，如果不关闭后面的主协程就会阻塞
+		close(c)
+	}()
+
+	for {
+		if data, ok := <-c; ok {
+			fmt.Println(data)
+		} else {
+			break
+		}
+	}
+
+	fmt.Println("Main goroutine ends...")
+}
