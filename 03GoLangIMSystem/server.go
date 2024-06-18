fd7cb2a0c94e117c6f8a2bd73fcfedc6c521f85e
Runze Yang <ryanyyyyyyy@163.com>
2024-06-18 23:31:47 +1000

    Golang update on the comments of the IMS server contents

--- 03GoLangIMSystem/server.go
+++ 03GoLangIMSystem/server.go
@@ -12,11 +12,11 @@ type Server struct {
 	Ip   string
 	Port int
 
-	//在线用户的列表
+	// 在线用户的列表
 	OnlineMap map[string]*User
 	mapLock   sync.RWMutex
 
-	//消息广播的channel
+	// 消息广播的channel
 	Message chan string
 }
 
@@ -37,7 +37,7 @@ func (this *Server) ListenMessager() {
 	for {
 		msg := <-this.Message
 
-		//将msg发送给全部的在线User
+		// 将msg发送给全部的在线User
 		this.mapLock.Lock()
 		for _, cli := range this.OnlineMap {
 			cli.C <- msg
@@ -54,17 +54,17 @@ func (this *Server) BroadCast(user *User, msg string) {
 }
 
 func (this *Server) Handler(conn net.Conn) {
-	//...当前链接的业务
-	//fmt.Println("链接建立成功")
+	// ...当前链接的业务
+	// fmt.Println("链接建立成功")
 
 	user := NewUser(conn, this)
 
 	user.Online()
 
-	//监听用户是否活跃的channel
+	// 监听用户是否活跃的channel
 	isLive := make(chan bool)
 
-	//接受客户端发送的消息
+	// 接受客户端发送的消息
 	go func() {
 		buf := make([]byte, 4096)
 		for {
@@ -79,37 +79,37 @@ func (this *Server) Handler(conn net.Conn) {
 				return
 			}
 
-			//提取用户的消息(去除'\n')
+			// 提取用户的消息(去除'\n')
 			msg := string(buf[:n-1])
 
-			//用户针对msg进行消息处理
+			// 用户针对msg进行消息处理
 			user.DoMessage(msg)
 
-			//用户的任意消息，代表当前用户是一个活跃的
+			// 用户的任意消息，代表当前用户是一个活跃的
 			isLive <- true
 		}
 	}()
 
-	//当前handler阻塞
+	// 当前handler阻塞
 	for {
 		select {
 		case <-isLive:
-			//当前用户是活跃的，应该重置定时器
-			//不做任何事情，为了激活select，更新下面的定时器
+			// 当前用户是活跃的，应该重置定时器
+			// 不做任何事情，为了激活select，更新下面的定时器
 
 		case <-time.After(time.Second * 300):
-			//已经超时
-			//将当前的User强制的关闭
+			// 已经超时
+			// 将当前的User强制的关闭
 
 			user.SendMsg("你被踢了")
 
-			//销毁用的资源
+			// 销毁用的资源
 			close(user.C)
 
-			//关闭连接
+			// 关闭连接
 			conn.Close()
 
-			//退出当前Handler
+			// 退出当前Handler
 			return //runtime.Goexit()
 		}
 	}
@@ -117,27 +117,27 @@ func (this *Server) Handler(conn net.Conn) {
 
 // Start 启动服务器的接口
 func (this *Server) Start() {
-	//socket listen
+	// socket listen
 	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
 	if err != nil {
 		fmt.Println("net.Listen err:", err)
 		return
 	}
-	//close listen socket
+	// close listen socket
 	defer listener.Close()
 
-	//启动监听Message的goroutine
+	// 启动监听Message的goroutine
 	go this.ListenMessager()
 
 	for {
-		//accept
+		// accept
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("listener accept err:", err)
 			continue
 		}
 
-		//do handler
+		// do handler
 		go this.Handler(conn)
 	}
 }
